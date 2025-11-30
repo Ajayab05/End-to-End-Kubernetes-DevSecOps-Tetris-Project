251035e29299b51a894f02c8608e4c0eb49bb766
Ajayab05 <53139108+Ajayab05@users.noreply.github.com>
2025-11-30 16:03:26 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,15 +1,15 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-bucket-15112025"
-    region         = "ap-south-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "use_logfile"
-    encrypt        = true
+    bucket       = "s3-bucket-15112025"
+    region       = "ap-south-1"
+    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    use_lockfile = true
+    encrypt      = true
   }
-  required_version = ">=0.13.0"
+  required_version = ">=1.14.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
+      version = ">= 5.49.0"
       source  = "hashicorp/aws"
     }
   }
